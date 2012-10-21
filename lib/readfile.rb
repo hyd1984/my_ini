d1e67dd12bc8152d3d847fb7ae6934a409929b95
aaa <dmcthehunter@gmail.com>
2012-10-21 11:31:07 +0800

    Parse parameter,value,section

--- lib/readfile.rb
+++ lib/readfile.rb
@@ -1,13 +1,16 @@
 def readfile(inifile)
     file=File.new("test.ini","r")
+    section_pat=/\[([A-Za-z0-9]*)\]/
+    comment_pat=/^;/
+    parameter_pat=/(\w*)=(\w*)/
     while(line=file.gets)
           line.chomp!.strip!
-          if(line =~/\[[A-Za-z0-9]*\]/)
-                 puts "section line"
-          elsif(line=~/^;/)
+          if(line =~section_pat)
+                 puts line[section_pat,1]
+          elsif(line=~comment_pat)
                  puts "comment line"
-          elsif(line=~/\w=\w/)
-                 puts "parameter line"
+          elsif(line=~parameter_pat)
+            puts "#{line[parameter_pat,1]}=#{ line[parameter_pat,2]}"
           else
                  puts "#{line}"
           end
