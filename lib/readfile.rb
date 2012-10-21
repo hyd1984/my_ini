eec2db6ca5780ffca58a1061a338672efe08fac9
aaa <dmcthehunter@gmail.com>
2012-10-21 11:07:28 +0800

    Judge the comment,section,parameter.

--- lib/readfile.rb
+++ lib/readfile.rb
@@ -1,7 +1,16 @@
 def readfile(inifile)
     file=File.new("test.ini","r")
     while(line=file.gets)
-          puts "#{line}"
+          line.chomp!.strip!
+          if(line =~/\[[A-Za-z0-9]*\]/)
+                 puts "section line"
+          elsif(line=~/^;/)
+                 puts "comment line"
+          elsif(line=~/\w=\w/)
+                 puts "parameter line"
+          else
+                 puts "#{line}"
+          end
     end
     file.close
 end
