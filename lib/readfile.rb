e55ce1b1a59375ac96ff606e17ea8ebc9af14567
aaa <dmcthehunter@gmail.com>
2012-10-21 11:49:37 +0800

    1.Store file's content in hash

--- lib/readfile.rb
+++ lib/readfile.rb
@@ -3,18 +3,24 @@ def readfile(inifile)
     section_pat=/\[([A-Za-z0-9]*)\]/
     comment_pat=/^;/
     parameter_pat=/(\w*)=(\w*)/
+    hash=Hash.new()
+    
     while(line=file.gets)
           line.chomp!.strip!
           if(line =~section_pat)
-                 puts line[section_pat,1]
+                 section=line[section_pat,1]
+                 hash[section]=Hash.new
           elsif(line=~comment_pat)
-                 puts "comment line"
+                 #puts "comment line"
           elsif(line=~parameter_pat)
-            puts "#{line[parameter_pat,1]}=#{ line[parameter_pat,2]}"
+                 #puts "#{line[parameter_pat,1]}=#{ line[parameter_pat,2]}"
+                 parameter,value=line[parameter_pat,1],line[parameter_pat,2]
+                 hash[section][parameter]=value
           else
                  puts "#{line}"
           end
     end
+  hash.each_pair {|key,value| print key," ",value,"\n"}
     file.close
 end
 
